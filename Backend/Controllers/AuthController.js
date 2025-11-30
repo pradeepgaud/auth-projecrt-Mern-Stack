3ca85ee68473af443be6fefc0e7331a9c8e0cb34
pradeepgaud <pradeepgaur1825@gmail.com>
2025-11-30 10:52:52 +0530

    Update AuthController.js

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -25,8 +25,8 @@ import transporter from "../Config/NodeMailer.js";
 
 //     res.cookie("token", token, {
 //       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//       secure: true",
+//       sameSite: "none",
 //       maxAge: 7 * 24 * 60 * 60 * 100,
 //     });
 
@@ -70,8 +70,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms (use *1000)
     });
 
@@ -138,8 +138,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 100,
     });
 
@@ -155,8 +155,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.json({ success: true, message: "Logged Out" });
   } catch (error) {
