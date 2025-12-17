c8c5f6f26992a26de0cb0b29827716c03edb5cda
pradeepgaud <pradeepsaini25604@gmail.com>
2025-12-18 01:03:46 +0530

    Disable email to fix register eggggrror

--- Backend/Config/Nodemailer.js
+++ Backend/Config/Nodemailer.js
@@ -1,37 +1,22 @@
-// // // import nodemailer from "nodemailer";
-
-// // // const transporter = nodemailer.createTransport({
-// // //   host :"smtp-relay.brevo.com",
-// // //   port:587,
-// // //   auth:{
-// // //     user:process.env.SMPT_USER,
-// // //     user:process.env.SMPT_PASS,
-
-// // //   }
-// // // });
-
-// // // export default transporter;
-
 // // import nodemailer from "nodemailer";
 
 // // const transporter = nodemailer.createTransport({
-// //   host: "smtp-relay.brevo.com",
-// //   port: 587,
-// //   auth: {
-// //     user: process.env.SMTP_USER,
-// //     pass: process.env.SMTP_PASS,
-// //   },
+// //   host :"smtp-relay.brevo.com",
+// //   port:587,
+// //   auth:{
+// //     user:process.env.SMPT_USER,
+// //     user:process.env.SMPT_PASS,
+
+// //   }
 // // });
 
 // // export default transporter;
 
-// // Config/NodeMailer.js
 // import nodemailer from "nodemailer";
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp-relay.brevo.com",
 //   port: 587,
-//   secure: false, // use TLS via STARTTLS on port 587
 //   auth: {
 //     user: process.env.SMTP_USER,
 //     pass: process.env.SMTP_PASS,
@@ -39,25 +24,18 @@
 // });
 
 // export default transporter;
-// import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp-relay.brevo.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS,
-//   },
-// });
+// Config/NodeMailer.js
+import nodemailer from "nodemailer";
 
-// // 👇 ADD THIS
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log("❌ SMTP ERROR:", error);
-//   } else {
-//     console.log("✅ SMTP READY");
-//   }
-// });
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // use TLS via STARTTLS on port 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-// export default transporter;
+export default transporter;
